f990ccfa0205022f65a07c646b0402bd72a2284d
Vadim Kuzmin <kuzminvadima@yandex.ru>
2023-08-24 20:49:21 +0300

    Removed Storyboard, added TabBarController, AppConfiguration

--- NFT Marketplace/SceneDelegate.swift
+++ NFT Marketplace/SceneDelegate.swift
@@ -3,5 +3,17 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {}
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let appConfig = AppConfiguration()
+        let tabBarController = TabBarController(appConfiguration: appConfig)
+        window.rootViewController = tabBarController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
 }
---
+++ NFT Marketplace/TabBarController/TabBarController.swift
@@ -0,0 +1,66 @@
+//
+//  TabBarController.swift
+//  NFT Marketplace
+//
+//  Created by Вадим Кузьмин on 24.08.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    let appConfiguration: AppConfiguration
+
+    init(appConfiguration: AppConfiguration) {
+        self.appConfiguration = appConfiguration
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        appConfiguration.profileViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.profileTabBarTitle,
+            image: UIImage(systemName: "person.crop.circle.fill"),
+            selectedImage: nil
+        )
+        appConfiguration.catalogViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.catalogTabBarTitle,
+            image: UIImage(systemName: "rectangle.stack.fill"),
+            selectedImage: nil
+        )
+        // TODO: Заменить изображение тут на значение из фигмы
+        appConfiguration.cartViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.cartTabBarTitle,
+            image: UIImage(systemName: "bag.fill"),
+            selectedImage: nil
+        )
+        appConfiguration.statisticViewController.tabBarItem = UITabBarItem(
+            title: S.TabBarController.statisticTabBarTitle,
+            image: UIImage(systemName: "flag.2.crossed.fill"),
+            selectedImage: nil
+        )
+
+        self.viewControllers = [
+            appConfiguration.profileViewController,
+            appConfiguration.catalogViewController,
+            appConfiguration.cartViewController,
+            appConfiguration.statisticViewController
+        ]
+
+        tabBar.isTranslucent = false
+        view.tintColor = .systemBlue
+        tabBar.backgroundColor = .white
+
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = appearance
+        }
+    }
+}
