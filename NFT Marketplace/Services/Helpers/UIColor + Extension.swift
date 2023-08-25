0c34a7520d74bc01dbaea3135197a80e57f03f02
Denis Chakyr <ChakyrIT@gmail.com>
2023-08-25 12:02:38 +0300

    Added asset-Colors from Figma

---
+++ NFT Marketplace/Services/Helpers/UIColor + Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor + Extension.swift
+//  FakeNFT
+//
+//  Created by Денис on 19.08.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let backgroundUni = UIColor(named: "BackgroundUni")
+    static let whiteDayNight = UIColor(named: "White[Day-Night]")
+    static let blackDayNight = UIColor(named: "Black[Day-Night]")
+    static let lightGreyDayNight = UIColor(named: "LightGrey[Day-Night]")
+    static let blackUni = UIColor(named: "BlackUni")
+    static let whiteUni = UIColor(named: "WhiteUni")
+    static let blueUni = UIColor(named: "BlueUni")
+    static let greyUni = UIColor(named: "GrayUni")
+    static let greenUni = UIColor(named: "GreenUni")
+    static let redUni = UIColor(named: "RedUni")
+    static let yellowUni = UIColor(named: "YellowUni")
+}
