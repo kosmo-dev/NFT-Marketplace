d7194e78c3beffc7792c301a80fc529cbe63ad6a
Denis Chakyr <ChakyrIT@gmail.com>
2023-09-05 00:28:17 +0300

    Added ScrollView on ProfileEditVC
    1) now user can write anything in textView and Screen will adapt to size of textField

--- NFT Marketplace/Scenes /Profile/ProfileEdit/ProfileEditViewController.swift
+++ NFT Marketplace/Scenes /Profile/ProfileEdit/ProfileEditViewController.swift
@@ -58,6 +58,15 @@ final class ProfileEditViewController: UIViewController {
         return stack
     }()
     
+    private lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        return sv
+    }()
+    
+    private var scrollViewBottomConstrait: NSLayoutConstraint!
+    
+    
     var presenter: ProfileEditPresenterProtocol?
     weak var delegate: ProfileEditViewControllerDelegate?
     
@@ -81,22 +90,49 @@ final class ProfileEditViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: nil)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .whiteDayNight
         profileAvatarView.delegate = self
         setupLayout()
-        // Добавляем распознаватель тапов для закрытия клавиатуры
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification: )),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification: )),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(textViewDidChange),
+                                               name: UITextView.textDidChangeNotification,
+                                               object: nil)
     }
     
     private func setupLayout() {
-        [doneButton, profileAvatarView, nameStackView, descriptionStackView, websiteStackView].forEach { view.addSubview($0) }
+        [doneButton, profileAvatarView, nameStackView, descriptionStackView, websiteStackView].forEach { scrollView.addSubview($0) }
+        view.addSubview(scrollView)
+        
         profileAvatarView.translatesAutoresizingMaskIntoConstraints = false
+        scrollViewBottomConstrait = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        scrollViewBottomConstrait.isActive = true
         
         NSLayoutConstraint.activate([
-            doneButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollViewBottomConstrait,
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            doneButton.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
             doneButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             profileAvatarView.topAnchor.constraint(equalTo: doneButton.bottomAnchor, constant: 22),
             profileAvatarView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -110,8 +146,14 @@ final class ProfileEditViewController: UIViewController {
             descriptionStackView.trailingAnchor.constraint(equalTo: nameStackView.trailingAnchor),
             websiteStackView.topAnchor.constraint(equalTo: descriptionStackView.bottomAnchor, constant: 24),
             websiteStackView.leadingAnchor.constraint(equalTo: descriptionStackView.leadingAnchor),
-            websiteStackView.trailingAnchor.constraint(equalTo: descriptionStackView.trailingAnchor)
+            websiteStackView.trailingAnchor.constraint(equalTo: descriptionStackView.trailingAnchor),
+            websiteStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20)
+            
         ])
+        
+        // Добавляем распознаватель тапов для закрытия клавиатуры
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
     
     private func updateUIWithProfile() {
@@ -135,6 +177,55 @@ final class ProfileEditViewController: UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    //MARK: - Methods for NSNotification
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        print("Показать")
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        let keyboardHeight = keyboardSize.height
+        
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        scrollView.scrollIndicatorInsets = scrollView.contentInset
+        
+        //Сдвиг скрола на высоту клавиатуры
+        var activeRect: CGRect? = nil
+        
+        if nameStackView.textView.isFirstResponder {
+            activeRect = nameStackView.frame
+        } else if descriptionStackView.textView.isFirstResponder {
+            activeRect = descriptionStackView.frame
+        } else if websiteStackView.textView.isFirstResponder {
+            activeRect = websiteStackView.frame
+        }
+        
+        if let activeRect = activeRect {
+            let rectInScrollView = scrollView.convert(activeRect, to: view)
+            let offset = rectInScrollView.maxY - (view.bounds.height - keyboardHeight)
+            let adjustedOffset = offset + 16
+            if offset > 0 {
+                scrollView.setContentOffset(CGPoint(x: 0, y: adjustedOffset), animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        print("Скрыть")
+        scrollViewBottomConstrait.constant = 0
+        view.layoutIfNeeded()
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
+    @objc func textViewDidChange(notification: NSNotification) {
+        let contentRect: CGRect = scrollView.subviews.reduce(into: .zero) { rect, view in
+            rect = rect.union(view.frame)
+        }
+        scrollView.contentSize = contentRect.size
     }
     
 }
